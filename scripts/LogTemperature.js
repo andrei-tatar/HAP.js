4ae758e9091632caed20c74bba6f607d42b3e6df
Andrei Tatar <ATATAR@isoftwaresystems.com>
2015-04-09 09:23:56 +0300

    Log temperature sample script updated added package.json

--- scripts/LogTemperature.js
+++ scripts/LogTemperature.js
@@ -5,29 +5,26 @@ module.exports = function(node, $pluginDir, util, log) {
         return new sqlite3.Database(path.join($pluginDir, "temperature.db"));
     });
 
-    var sensor = node.device('temp_birou');
-    if (!sensor) return;
+    node.device('temp_birou', function (sensor) {
+        sensor.on('temperature', function (value) {
+            console.log("Temperature : " + value);
+            var tableName = "Temp1";
+            var time = Math.round(Date.now()/1000);
 
-    sensor.on('temperature', function (value) {
-        console.log("Temperature : " + value);
-        var tableName = "Temp1";
-        var time = Math.round(Date.now()/1000);
-
-        db.value.serialize(function() {
-            db.value.run("begin transaction");
-            db.value.run("create table if not exists " + tableName + " (time INTEGER, value REAL)");
-            db.value.run("insert into " + tableName + " values (?, ?)", [time, value]);
-            db.value.run("commit");
+            db.value.serialize(function() {
+                db.value.run("begin transaction");
+                db.value.run("create table if not exists " + tableName + " (time INTEGER, value REAL)");
+                db.value.run("insert into " + tableName + " values (?, ?)", [time, value]);
+                db.value.run("commit");
+            });
         });
     });
 
-    node.on('device_discovered', function (device) {
-        log.i('[NODE]Discovered id:'+device.id+', name:'+device.name+', type:'+device.type+', addr:'+device.address);
+    node.on('device_add', function (device) {
+        log.i('[NODE]id:'+device.id+', name:'+device.name+', type:'+device.type+', addr:'+device.address);
 
-        device.on('available', function(available) {
-            device.get('/heap', function (error, response, body) {
-                log.i("Heap Size ("+device.name+"):"+body);
-            });
+        device.get('/heap', function (statusCode, body) {
+            log.i("Heap Size (" + device.name + "):" + body);
         });
     });
 };
